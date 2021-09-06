e7e3be6a6fe97457ef94e5ed3dff43377992da09
Thobif <jirayootpandee537@gmail.com>
2021-09-06 15:29:23 +0700

    initial commit

---
+++ first.js
@@ -0,0 +1,18 @@
+
+function writeSomeText(){
+    document.getElementById("demo").innerHTML="This is My Family";
+    document.getElementById("demo2").innerHTML="This is a Fish";
+window.alert("This test was writting by Js");
+
+}
+
+writeSomeText();
+
+/*var myName = "Elon MAsk";
+var myname = "Mask Sakaberk";
+var Firstname = "Jack Ma";
+console.log(myName);            
+console.log(myname); */
+
+//console.log(Firstname);
+
